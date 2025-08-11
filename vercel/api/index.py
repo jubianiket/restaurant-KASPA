953852bab1a173f4401389e3244f3e5b53372cff
jubianiket <jubi.aniket@gmail.com>
2025-08-11 08:27:43 +0530

    removed vercel folder

--- vercel/api/index.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def handler(request, context):
-    # Example: return a simple JSON response
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": json.dumps({"message": "Hello from Vercel Python Serverless Function!"})
-    }
